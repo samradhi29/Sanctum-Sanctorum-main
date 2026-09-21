c9fb96769e1241e310560e369365cae767b9918e
samradhiR <samradhi4320@gmail.com>
2026-09-21 19:35:48 +0530

    Add psycopg driver and conditional connect_args for Postgres deployment

--- app/db.py
+++ app/db.py
@@ -6,7 +6,9 @@ from sqlalchemy.orm import DeclarativeBase, Session, sessionmaker
 
 DATABASE_URL = os.getenv("SANCTUM_DATABASE_URL", "sqlite:///./sanctum.db")
 
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+connect_args = {"check_same_thread": False} if DATABASE_URL.startswith("sqlite") else {}
+
+engine = create_engine(DATABASE_URL, connect_args=connect_args)
 SessionLocal = sessionmaker(bind=engine, autoflush=False, expire_on_commit=False)
 
 
